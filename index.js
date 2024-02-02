1b28888f359a578e2df61e14a7d66c3c98deddee
Victor <victorac2324@gmail.com>
2024-02-02 18:53:12 -0500

    used for loop to countdown to 10

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+
+const cards = ["Charlie", "Samip", "Ali"] 
+
+
+function writeCards(cards, event){
+    const birth =[] 
+
+    for (let x = 0; x < cards.length; x++){
+        birth.push(`Thank you, ${cards[x]}, for the wonderful ${event} gift!`)   
+    }
+
+   return birth
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+
+     return gifts;
+}
+
+wrapGifts(gifts)
+
+function countDown(number){
+    for (let n = number; n >= 0; n--)
+    console.log(n)
+}
+
